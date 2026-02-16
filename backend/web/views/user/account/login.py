b4aebc050398c32a9aba3dd091fee25c9954786c
zhizhibucheng <2303176225@qq.com>
2026-02-16 17:38:27 +0800

    登录功能

--- backend/web/views/user/account/login.py
+++ backend/web/views/user/account/login.py
@@ -17,7 +17,7 @@ class LoginView(APIView):
                 })
             user = authenticate(username=username, password=password)
             if user:
-                user_profile = UserProfile.objects.get(username=username)
+                user_profile = UserProfile.objects.get(user=user)
                 refresh = RefreshToken.for_user(user) #生成jwt
                 response = Response({
                     'result': 'success',
--- frontend/src/stores/user.js
+++ frontend/src/stores/user.js
@@ -2,11 +2,11 @@ import {defineStore}  from "pinia";
 import {ref} from "vue";
 
 export const useUserStore =defineStore('user',()=>{
-    const id = ref(1)
-    const username = ref('zzbc')
-    const photo = ref('http://127.0.0.1:8000/media/user/photos/default.png')
-    const profile = ref('11')
-    const accessToken = ref('11')
+    const id = ref(0)
+    const username = ref('')
+    const photo = ref('')
+    const profile = ref('')
+    const accessToken = ref('')
 
     function isLogin(){
         return !!accessToken.value
@@ -17,7 +17,7 @@ export const useUserStore =defineStore('user',()=>{
     }
 
     function setUserInfo(data){
-        id.value = data.id
+        id.value = data.user_id
         username.value = data.username
         photo.value=data.photo
         profile.value=data.profile
